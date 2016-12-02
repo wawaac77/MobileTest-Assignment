8768a39936a3ae014f05d0ab0b0c9b30a46c7f15
wawaac77 <wawaac@gmail.com>
2016-12-02 12:37:25 +0800

    Version 1
    Test version

---
+++ Core/Models/Meal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Core
+{
+	public class Meal
+	{
+		public String Title {get;set;}
+		public String Location {get;set;}
+		public DateTime CreatedDate {get;set;}
+		public byte[] ImageSource {get;set;}
+
+		public Meal ()
+		{
+			CreatedDate = DateTime.UtcNow;
+		}
+	}
+}
+
